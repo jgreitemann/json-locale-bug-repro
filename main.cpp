a49741a626145b5a4ff4b6962e3c90b84b851840
Jonas Greitemann <jgreitemann@gmail.com>
2026-05-31 12:47:50 +0200

    Mitigate bug by setting thread-local locale (POSIX)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 #include <thread>
+#include <type_traits>
+
+#include <locale.h>
 
 #include <nlohmann/json.hpp>
 
@@ -10,7 +14,23 @@ const char *kJson = R"({"val": 99.123456})";
 static constexpr double kExpected = 99.123456;
 static constexpr int kMaxSwitches = 10'000'000;
 
+struct LocaleGuard {
+public:
+  explicit LocaleGuard(int mask, const char *locale) noexcept
+      : m_Loc(newlocale(mask, locale, (locale_t)0)) {
+    uselocale(m_Loc.get());
+  }
+
+private:
+  struct Deleter {
+    void operator()(locale_t l) noexcept { freelocale(l); }
+  };
+  std::unique_ptr<std::remove_pointer_t<locale_t>, Deleter> m_Loc{};
+};
+
 void worker(std::stop_token stop) {
+  LocaleGuard c_locale(LC_NUMERIC, "C");
+
   for (int i = 0; !stop.stop_requested(); ++i) {
     json j = json::parse(kJson);
     double val = j.value("val", 0.0);
